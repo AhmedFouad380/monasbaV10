54890642db21c7fb50aeef6e1dda69fbebaa738b
AhmedFouad380 <36769581+AhmedFouad380@users.noreply.github.com>
2024-01-28 21:18:16 +0200

    a

--- app/Http/Resources/Api/NotificationResource.php
+++ app/Http/Resources/Api/NotificationResource.php
@@ -19,7 +19,7 @@ class NotificationResource extends JsonResource
             'id'=>$this->id,
             'name'=>$this->name,
             'type'=>$this->type,
-            'user_id'=>(int) $this->UserID($this->chat_id) ? $this->UserID($this->chat_id) : null,
+            'user_id'=>(int) $this->UserID($this->chat_id) ? $this->UserID($this->chat_id) : $this->user_id,
             'product_id'=>$this->product_id ? $this->product_id : null,
             'date'=>Carbon::parse($this->created_at)->diffForHumans(),
             'image'=>$this->Product ?  $this->Product->image : asset('logo/logo.png')
